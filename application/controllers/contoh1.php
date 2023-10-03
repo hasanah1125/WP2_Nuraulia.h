3a08b77805166127910ab59cf02bdf6cd682e767
hasanah1125 <nurauliahasanah1999@gmail.com>
2023-10-03 19:43:45 +0700

    Upload Awal Project

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"Nama saya Nur Aulia Hasanah
+             saya tinggal di desa kapur
+             olahraga yang saya sukai adalah
+             Bersepeda keliling komplek";
+    }
+}
