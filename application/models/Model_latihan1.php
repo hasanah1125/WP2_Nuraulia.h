ff660c3abff89381c57076fa60c34cc9060be861
hasanah1125 <nurauliahasanah1999@gmail.com>
2023-10-08 01:56:49 +0700

    upload tugas pertemuan 3

--- application/models/Model_latihan1.php
+++ application/models/Model_latihan1.php
@@ -1,14 +1,16 @@
 <?php
-class Model_latihan extends CI_Model
+class Model_latihan1 extends CI_Model
 {
     //membuat variable untuk menampung nilai
     public $nilai1, $nilai2, $hasil;
+    
     //method penjumlahan
-    public function jumlah ($n1= null, $n2 = null)
-   {
-            $this->nilai = $n1;
-            $this->nilai = $n2;
-            $this->hasil = $this->nilai1 + $this->nilai2;
-            return $this->hasil;
-   }
-}          
+    public function jumlah($n1 = null, $n2 = null)
+    {
+        $this->nilai1 = $n1;
+        $this->nilai2 = $n2;
+        $this->hasil = $this->nilai1 + $this->nilai2;
+        return $this->hasil;
+    }
+}
+
