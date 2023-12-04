3659c6b1c47d944dbb68eb1c5a2f8bca3e4cd2e9
hasanah1125 <nurauliahasanah1999@gmail.com>
2023-12-04 22:09:01 +0700

    tugas pertemuan 11

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -29,6 +29,12 @@ Booking</div>
                         <!-- Nav Item - Dashboard -->
                         <li class="nav-item">
                             <a class="nav-link pb-0" href="<?=
+base_url('buku/kategori'); ?>">
+                                <i class="fa fa-fw fa book"></i>
+                                <span>Kategori Buku</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link pb-0" href="<?=
 base_url('buku'); ?>">
                                 <i class="fa fa-fw fa book"></i>
                                 <span>Data Buku</span></a>
